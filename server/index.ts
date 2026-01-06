5936d086de9a2627ce494302f29c973ff9c009da
Syam Sundar Kudumala <syamsundarkudumala@gmail.com>
2026-01-06 18:27:55 +0530

    Fix TypeScript production build for Render

--- server/index.ts
+++ server/index.ts
@@ -92,7 +92,8 @@ app.use((req, res, next) => {
   }
 
   // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
+
 
 httpServer.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
